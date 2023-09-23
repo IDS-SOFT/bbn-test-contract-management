078c5a868f8e8d408e53814ad1142a92ab6259f1
NikinikitaN <119474174+NikinikitaN@users.noreply.github.com>
2023-09-23 18:39:49 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,8 @@ import "@nomiclabs/hardhat-waffle";
 //import "@typechain/hardhat";
 //import "hardhat-gas-reporter";
 //import "solidity-coverage";
-//import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
 
 
 dotenv.config();
@@ -15,7 +16,7 @@ dotenv.config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
